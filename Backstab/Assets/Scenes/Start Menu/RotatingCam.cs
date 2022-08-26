b489ec96a55b8a2be6a499f1b09fc5e2eb888646
Quinn Horton <106427075+q-horton@users.noreply.github.com>
2022-08-27 03:22:33 +1000

    Added Title Screen
    Begun work on the title screen for the game.

---
+++ Backstab/Assets/Scenes/Start Menu/RotatingCam.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCam : MonoBehaviour
+{
+    public Camera camera;
+    public int orbitRange = 100;
+    public int orbitPeriod = 60;
+    public int viewAngle = 20;
+
+    float theta;
+    float x;
+    float y;
+    float z;
+    float phi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theta = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theta += Time.deltaTime * 2 * Mathf.PI / orbitPeriod;
+        x = orbitRange * Mathf.Cos(theta);
+        y = orbitRange * Mathf.Tan(viewAngle * Mathf.PI / 180);
+        z = orbitRange * Mathf.Sin(theta);
+        phi = 270 - 180 * theta / Mathf.PI;
+
+        camera.transform.position = new Vector3(x, y, z);
+        camera.transform.rotation = Quaternion.Euler(viewAngle, phi, 0);
+        
+    }
+}
