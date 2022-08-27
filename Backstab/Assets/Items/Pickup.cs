52344167113855babfec00dd1240cf95276130b2
Kevin <hasakev@gmail.com>
2022-08-27 13:13:41 +1000

    Inventory UI

--- Backstab/Assets/Items/Pickup.cs
+++ Backstab/Assets/Items/Pickup.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     public GameObject pickup;
+    private float MoveSpeed = 100f;
+    Transform target;
+    
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
             PlayerStats ps = other.gameObject.GetComponent<PlayerStats>();
             if (!ps.isInventoryFull()) {
                 ps.inventory[ps.getFirstOpenInvSlot()] = pickup;
-                Destroy(pickup);
+                pickup.transform.position = target.position;
             }
         }
     }
@@ -19,7 +22,7 @@ public class Pickup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("InvenSlot").transform;
     }
 
     // Update is called once per frame
