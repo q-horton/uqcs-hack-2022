b7c8fc74e57f002b02b50919d15f83f9415cdb89
Quinn Horton <106427075+q-horton@users.noreply.github.com>
2022-08-27 13:16:30 +1000

    Updated Start Menu
    Updated Start Menu Pointer

--- Backstab/Assets/Scenes/Start Menu/PlayButton.cs
+++ Backstab/Assets/Scenes/Start Menu/PlayButton.cs
@@ -7,6 +7,6 @@ public class PlayButton : MonoBehaviour
 {
     public void ChangeScene()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("SwarmArena");
     }
 }
