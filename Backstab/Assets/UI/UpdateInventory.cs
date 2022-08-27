4efa89c235c0c1116334ebb9ef0127bf5cfa4501
Quinn Horton <106427075+q-horton@users.noreply.github.com>
2022-08-27 21:34:56 +1000

    Update Errors

--- Backstab/Assets/UI/UpdateInventory.cs
+++ Backstab/Assets/UI/UpdateInventory.cs
@@ -6,16 +6,12 @@ using UnityEngine.UI;
 public class UpdateInventory : MonoBehaviour
 {
     public PlayerStats stats;
-<<<<<<< Updated upstream
     public Text jump;
     public Text regen;
     public Text strength;
     public Text speed;
     public Text armour;
     public Text reach;
-=======
-    public GameObject inventory;
->>>>>>> Stashed changes
     
     void Start() {
         jump = GameObject.Find("Score 1").GetComponent<Text>();
@@ -29,18 +25,11 @@ public class UpdateInventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
         jump.text = stats.inventory[0].ToString();
         regen.text = stats.inventory[1].ToString();
         strength.text = stats.inventory[2].ToString();
         speed.text = stats.inventory[3].ToString();
         armour.text = stats.inventory[4].ToString();
         reach.text = stats.inventory[5].ToString();
-=======
-        for (int i = 0; i < stats.inventory.Length; i++) {
-            Text txt = inventory.GetComponent<Text>();
-            txt.text = stats.inventory[0].ToString();
-        }
->>>>>>> Stashed changes
     }
 }
